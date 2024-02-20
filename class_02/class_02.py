44cc6f08324240220f309e32e913b3e0931c2e11
afzalfaizi <afzaalm993@gmail.com>
2024-02-20 18:29:09 +0500

    String methods(string, fstring)

---
+++ class_02/class_02.py
@@ -0,0 +1,10 @@
+# string methods in Python
+
+userName : str = "Muhammad Afzal" # String with Double Quote
+print(userName)
+
+name : str = 'afzal faizi' # string with single quote
+print(name)
+
+myName : str = '''afzal faizi''' # string with triple quote
+print(myName)
