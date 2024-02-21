2c62663bd22a4d0ada914dac3ae9d97f6a044360
afzalfaizi <afzaalm993@gmail.com>
2024-02-21 14:31:42 +0500

    string methods(fstring, jinja)

--- class_02/class_02.py
+++ class_02/class_02.py
@@ -8,3 +8,21 @@ print(name)
 
 myName : str = '''afzal faizi''' # string with triple quote
 print(myName)
+
+## Define multiline string "" "" "" triple line string
+studentName : str = "Muhammad Afzal"
+fName : str = "Iqbal Yousaf"
+age : int = 26
+qualifaction : str = "Master of Computer Science and Engineering."
+address : str = "Chak No 5 Kalan G.B P/O same Tahsil & District Nankana Sahib"
+
+card : str = f"""
+PIAIC Student Card
+Student Name : {studentName}
+First Name : {fName}
+Age : {age}
+Qualification : {qualifaction}
+Address : {address}
+
+"""
+print(card)
