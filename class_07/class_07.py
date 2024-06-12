7f5f03e384f6b4006fb92406e43988bb4fb78a45
afzalfaizi <afzaalm993@gmail.com>
2024-06-12 12:30:01 +0500

    Dictionary In  Python

---
+++ class_07/class_07.py
@@ -0,0 +1,5 @@
+# Dictionary, Set, Tuple
+# Dictionary
+# A dictionary is a collection of key-value pairs. It is mutable, meaning it can be changed
+# after it is created.
+# Dictionaries are defined using curly brackets {}
