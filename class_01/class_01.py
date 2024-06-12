815bbf0330cfa96575ea4dd7d2bc07db1eb121df
afzalfaizi <afzaalm993@gmail.com>
2024-06-12 11:22:26 +0500

    Variables in Python

--- class_01/class_01.py
+++ class_01/class_01.py
@@ -1,6 +1,17 @@
+# Declare a variable named 'myName' of type string and assign it the value "Muhammad Afzal"
 myName :str  = "Muhammad Afzal"
+
+# Declare a variable named 'fName' of type string and assign it the value "Iqbal Yousaf"
 fName : str = "Iqbal Yousaf"
+
+# Print the value of 'myName' to the console
 print(myName)
+
+# Print the value of 'fName' to the console
 print(fName)
+
+# Print the 'type' function to the console, which is a built-in function in Python
 print(type)
+
+# Print the 'id' function to the console, which is a built-in function in Python that returns the unique identifier of an object
 print(id)
