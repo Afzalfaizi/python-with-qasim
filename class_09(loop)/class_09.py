3017a3080a604c8c1f42720eb3e9a4330ec729d6
afzalfaizi <afzaalm993@gmail.com>
2024-07-17 22:59:47 +0500

    User Input Practice

---
+++ class_09(loop)/class_09.py
@@ -0,0 +1,9 @@
+name : str = input("What is your name? : \t")
+
+accountNum : int = input("Please Add Your Account Number : \t" )
+print("Welcome to the ATM Machine Mr/Mrs " + name + " Your Account Number is "
++ accountNum + "\n")
+
+print(type(name))
+
+print(f"Welcome dear User Mr/Miss {name}!")
