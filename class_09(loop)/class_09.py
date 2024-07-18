e151a26b9874e690e22d48b1aeb6f4263a56f4e3
afzalfaizi <afzaalm993@gmail.com>
2024-07-18 15:19:11 +0500

    User Input Using System argv

--- class_09(loop)/class_09.py
+++ class_09(loop)/class_09.py
@@ -1,9 +1,23 @@
-name : str = input("What is your name? : \t")
+import sys
 
-accountNum : int = input("Please Add Your Account Number : \t" )
-print("Welcome to the ATM Machine Mr/Mrs " + name + " Your Account Number is "
-+ accountNum + "\n")
+print("Line No 1")
+print("Line No 2")
 
-print(type(name))
+print(type(sys.argv))
+print(sys.argv)
 
-print(f"Welcome dear User Mr/Miss {name}!")
+
+
+
+
+
+
+
+# name : str = input("What is your name? : \t")
+# accountNum : int = input("Please Add Your Account Number : \t" )
+# print("Welcome to the ATM Machine Mr/Mrs " + name + " Your Account Number is "
+# + accountNum + "\n")
+
+# print(type(name))
+
+# print(f"Welcome dear User Mr/Miss {name}!")
