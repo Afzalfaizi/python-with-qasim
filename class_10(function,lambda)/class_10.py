67fa13e22c5c99da674605a93708cce062ae2b6d
afzalfaizi <afzaalm993@gmail.com>
2024-07-28 18:19:46 +0500

    functions in python

--- class_10(function,lambda)/class_10.py
+++ class_10(function,lambda)/class_10.py
@@ -5,3 +5,9 @@ myName()
 def fName():
     print("my father name is Iqbal yousaf")
 fName()
+
+def add_two_numbers():
+    num1 = 10
+    num2 = 20
+    print("sum of two numbers is ", num1 + num2)
+add_two_numbers()
