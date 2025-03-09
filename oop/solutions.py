f6f2f279e4f36d38c32afa97808f3c02d2338ca1
afzalfaizi <afzaalm993@gmail.com>
2025-03-09 12:52:02 +0500

    Update kernel and execution counts in class notebooks

---
+++ oop/solutions.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        
+    
+my_car = Car("Honda", "Civic")
+print(my_car.brand)
+print(my_car.model)
+
