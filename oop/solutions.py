96cb5b0995e9d097a6cdb15bafe8648d8af8d9f8
afzalfaizi <afzaalm993@gmail.com>
2025-03-09 12:57:05 +0500

    feat: Add Car class with basic attributes and display method

--- oop/solutions.py
+++ oop/solutions.py
@@ -3,8 +3,13 @@ class Car:
         self.brand = brand
         self.model = model
         
+    def display_details(self):
+        return f"{self.brand} {self.model}"        
+        
     
 my_car = Car("Honda", "Civic")
 print(my_car.brand)
 print(my_car.model)
+print(my_car.display_details())
 
+# commit: feat: Add Car class with basic attributes and display method
